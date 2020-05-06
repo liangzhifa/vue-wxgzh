8147dd3b976185a55d50b9cad681f77bc0711141
liangzhifa <877657682@qq.com>
2020-05-06 14:02:41 +0800

    添加工作经历

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
     </keep-alive>
     <van-tabbar route>
       <van-tabbar-item replace to="/me" icon="home-o">
-        我
+        个人信息
       </van-tabbar-item>
       <van-tabbar-item replace to="/experience" icon="apps-o">
-        项目
+        项目介绍
       </van-tabbar-item>
     </van-tabbar>
   </div>
